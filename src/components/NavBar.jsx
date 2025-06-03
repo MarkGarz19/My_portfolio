ddbb87fc2cf13ac5d407fb3cbc3b1bd254d8864d
Markus Garzon Bohorquez <markusbohorquez23@gmail.com>
2025-06-03 17:48:01 +0200

    Cuarto commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { cn } from "@/libs/utils";
+import { Menu,X } from "lucide-react";
 import { useEffect, useState } from "react";
 const navItems = [
     {name:"Home", href:"#hero"},
@@ -10,7 +11,7 @@ const navItems = [
 
 export const NavBar = () =>{
     const [isScrolled, setIsScrolled]= useState(false);
-    const [isMenuOpen, setIsMenuOpen]= useState(true);
+    const [isMenuOpen, setIsMenuOpen]= useState(false);
 
     useEffect( () => {
         const handleScroll = () => {
@@ -42,6 +43,11 @@ export const NavBar = () =>{
 
 
             {/* mobile nav */}
+            <button onClick={()=>setIsMenuOpen((prev)=>!prev)} 
+            className="md:hidden p-2 text-foreground z-50"
+            aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
+                {isMenuOpen ? <X size={24} />: <Menu size={24} />}
+            </button>
             <div className={cn("fixed inset-0 bg-background/90 backdrop-blur-md z-40 flex flex-col items-center justify-center",
                 "transition-all duration-200 md:hidden",
                 isMenuOpen ? "opacity-100 pointer-events-auto"
