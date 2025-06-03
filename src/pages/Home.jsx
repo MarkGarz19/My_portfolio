2d235ea0aa3c6865e8e1d38ce5f88bac652c4f67
Markus Garzon Bohorquez <markusbohorquez23@gmail.com>
2025-06-03 19:22:17 +0200

    Quinto commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import {StarBackground} from "@/components/StarBackground"
 import { NavBar } from "@/components/NavBar";
-import { HeroSection } from "@/components/HeroSection";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -14,6 +15,7 @@ export const Home = () => {
         {/* Main Content */}
         <main>
             <HeroSection />
+            <AboutSection />
         </main>
 
         {/* Footer */}
